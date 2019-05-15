94a33f492c9f51e5bb0306884b043081bcf0479a
Gary Meehan <garymeehan@outlook.com>
2019-05-15 16:54:50 +0100

    init

---
+++ components/links.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Links = () => (
+  <ul>
+    <li>
+      <Link href="/">
+        <a>Default SEO</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/article">
+        <a>Article SEO</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/book">
+        <a>Book SEO</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/profile">
+        <a>Profile SEO</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/overridden">
+        <a>Overridden Seo</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/jsonld">
+        <a>All JSON-LD</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/updated-title-template">
+        <a>Updated Title Template</a>
+      </Link>
+    </li>
+  </ul>
+);
+
+export default Links;
